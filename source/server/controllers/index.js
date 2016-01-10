0401f5e212b35d2629b890821f08ef0aa38b7751
dilyantraykov <dtraykov94@gmail.com>
2016-01-10 20:26:26 +0200

    Added initial code - games & some authentication

---
+++ source/server/controllers/index.js
@@ -0,0 +1,7 @@
+var usersController = require('../controllers/usersController');
+var gamesController = require('../controllers/gamesController');
+
+module.exports = {
+    users: usersController,
+    games: gamesController
+}
