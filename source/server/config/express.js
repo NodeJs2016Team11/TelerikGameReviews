5b28ce5af0bd4df36a5d1e64ce09d103af7295e8
Marto Vesheff <Martin.Veshev@gmail.com>
2016-01-14 15:03:04 +0200

    Fixed some styles and added user to navbar.

--- source/server/config/express.js
+++ source/server/config/express.js
@@ -20,13 +20,35 @@ module.exports = function (app, config) {
     saveUninitialized: true,
     resave: true
   }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(express.static(config.rootPath + '/public'));
   // app.use(stylus.middleware({
   //   src: config.rootPath + '/public',
   //   compile: function (str, path) {
   //     return stylus(str).set('filename', path);
   //   }
   // }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(express.static(config.rootPath + '/public'));
+  app.use(function(req, res, next) {
+        if (req.session.error) {
+            var msg = req.session.error;
+            req.session.error = undefined;
+            app.locals.errorMessage = msg;
+        }
+        else {
+            app.locals.errorMessage = undefined;
+        }
+
+        next();
+    });
+    app.use(function(req, res, next) {
+        if (req.user) {
+            app.locals.currentUser = req.user;
+        }
+        else {
+            app.locals.currentUser = undefined;
+        }
+
+        next();
+    });
 };
